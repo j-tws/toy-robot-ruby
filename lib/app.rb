76b2ae0ef60e0f4e5fa0f2218388c9aeb1214ae9
Justin Tan <justintws10@gmail.com>
2023-11-27 12:33:56 +1100

    Chore: refactor display to be more generic
    The display should not know anything about robot or board.
    By refactoring the display to be more generic it makes it
    more generic and reusable for other cases like other games
    or application.

--- lib/app.rb
+++ lib/app.rb
@@ -2,7 +2,39 @@ require_relative 'display'
 require_relative 'simulator'
 
 class App
-  attr_reader :display, :simulator
+  # attr_reader :display, :simulator
+  MESSAGES = {
+    start_menu: <<~START_MENU,
+      ---------------------------------------------------
+      |          WELCOME TO TOY ROBOT SIMULATOR         |
+      ---------------------------------------------------
+
+
+      First by starting up your board!
+    START_MENU
+    enter_board_rows: 'Enter the amount of rows you want to have for your board:',
+    enter_board_cols: 'Enter the amount of columns you want to have for your board:',
+    enter_robot_start_col: "Let's start which column do you want to place your robot:",
+    enter_robot_start_row: 'Which row do you want to place your robot:',
+    enter_robot_bearing: "Enter where you want your robot to face (only enter 'north', 'south', 'east', 'west'):",
+    enter_command: 'Enter your commands here:',
+    build_board_complete: 'Here is how your board look like',
+    start_place_robot: 'Now place where you want your robot to start',
+    robot_starting_position_message: 'This is where your robot start',
+    run_command_intro_message: "Now the fun begins\nYou can enter a series of commands where you will see your robot moving around the board",
+    command_instructions: <<~COMMAND_INSTRUCTIONS,
+      Commands can be only either:
+      R, which means ask the robot to turn right from where it is currently facing
+      L, which means ask the robot to turn left from where it is currently facing
+      A, which means ask the robot to advance one cell from where it is facing
+      \n
+      Example command: RLAR
+      It means it command the robot to turn right, turn left, advance a step, and finish by turning right
+    COMMAND_INSTRUCTIONS
+    current_command: 'Current command:',
+    robot_coordinates: 'Robot coordinates:',
+    robot_bearing: 'Robot bearing'
+  }.freeze
 
   def initialize
     @display = Display.new
@@ -11,59 +43,76 @@ class App
 
   def build_board
     loop do
-      board_rows_input = @display.get_board_rows
-      baord_columns_input = @display.get_board_columns
-      @simulator.setup_board(board_rows_input, baord_columns_input)
+      @display.print(MESSAGES[:enter_board_cols])
+      board_columns_input = @display.get_input_to_int
 
-      @display.build_board_complete_message
-      @display.print_board(@simulator.board.board_with_axis)
+      @display.print(MESSAGES[:enter_board_rows])
+      board_rows_input = @display.get_input_to_int
+
+      @simulator.setup_board(board_rows_input, board_columns_input)
+
+      @display.print(MESSAGES[:build_board_complete])
+      @display.draw(@simulator.board.board_with_axis)
 
       return if @simulator.board
     rescue ArgumentError => e
-      @display.print_error(e)
+      @display.print(e)
       next
     end
   end
 
   def place_robot
-    @display.start_place_robot_message
+    @display.print(MESSAGES[:start_place_robot])
 
     loop do
-      robot_column_input = @display.get_robot_start_column
-      robot_row_input = @display.get_robot_start_row
-      robot_bearing_input = @display.get_robot_start_bearing
+      @display.print(MESSAGES[:enter_robot_start_col])
+      robot_column_input = @display.get_input_to_int
+
+      @display.print(MESSAGES[:enter_robot_start_row])
+      robot_row_input = @display.get_input_to_int
+
+      @display.print(MESSAGES[:enter_robot_bearing])
+      robot_bearing_input = @display.get_input_to_sym
+
       @simulator.place(x: robot_column_input, y: robot_row_input, direction: robot_bearing_input)
 
-      @display.show_robot_starting_position_message
-      @display.print_board(@simulator.board.board_with_axis)
+      @display.print(MESSAGES[:robot_starting_position_message])
+      @display.draw(@simulator.board.board_with_axis)
 
       return if @simulator.robot.bearing
     rescue ArgumentError => e
-      @display.print_error(e)
+      @display.print(e)
       next
     end
   end
 
   def run_commands
-    @display.show_run_command_intro_message
+    @display.print(MESSAGES[:run_command_intro_message])
 
     loop do
-      @display.show_command_instructions
+      @display.print(MESSAGES[:command_instructions])
+      @display.print(MESSAGES[:enter_command])
 
-      commands = @display.get_user_commands
+      commands = @display.get_input_as_str
       system 'clear'
-      @simulator.evaluate(commands) { |outcome| @display.print_outcome(@simulator.robot, @simulator.board, outcome) }
-      @display.print_board(@simulator.board.board_with_axis)
-      @display.enter_another_command
+
+      @simulator.evaluate(commands) do |command|
+        @display.print("#{MESSAGES[:current_command]} #{command}\n")
+        @display.draw(@simulator.board.board_with_axis)
+        @display.print("#{MESSAGES[:robot_coordinates]} #{@simulator.robot.coordinates}")
+        @display.print("#{MESSAGES[:robot_bearing]} #{@simulator.robot.bearing}")
+      end
+
+      @display.print(MESSAGES[:enter_command])
     rescue ArgumentError, Simulator::HitBoardBoundaryError => e
-      @display.print_error(e)
+      @display.print(e)
       next
     end
   end
 
   def run
     loop do
-      @display.start_menu
+      @display.print(MESSAGES[:start_menu])
       build_board
       place_robot
       run_commands
--- lib/display.rb
+++ lib/display.rb
@@ -1,108 +1,28 @@
 class Display
-  def start_menu
-    puts <<~START_MENU
-      ---------------------------------------------------
-      |          WELCOME TO TOY ROBOT SIMULATOR         |
-      ---------------------------------------------------
-
-
-      First by starting up your board!
-    START_MENU
-  end
-
-  def parse_input_to_integer
-    input = gets.chomp
-    if input == 'exit'
-      exit(true)
-    elsif input.scan(/\D/).any?
-      input
-    else
-      input.to_i
-    end
-  end
-
-  def get_board_rows
-    puts 'Enter the amount of rows you want to have for your board:'
-    parse_input_to_integer
-  end
-
-  def get_board_columns
-    puts 'Enter the amount of columns you want to have for your board:'
-    parse_input_to_integer
-  end
-
-  def get_robot_start_row
-    puts 'Which row do you want to place your robot:'
-    parse_input_to_integer
-  end
-
-  def get_robot_start_column
-    puts "let's start which column do you want to place your robot:"
-    parse_input_to_integer
-  end
-
-  def get_robot_start_bearing
-    puts "Enter where you want your robot to face (only enter 'north', 'south', 'east', 'west'):"
-    exit_or_proceed(gets.chomp).to_sym
-  end
-
-  def get_user_commands
-    puts 'Enter your commands here:'
-    exit_or_proceed(gets.chomp)
-  end
-
-  def build_board_complete_message
-    puts 'Here is how your board look like'
-  end
-
-  def start_place_robot_message
-    puts 'Now place where you want your robot to start'
-  end
-
-  def show_robot_starting_position_message
-    puts 'This is where your robot start'
-  end
-
-  def show_run_command_intro_message
-    puts 'Now the fun begins'
-    puts 'You can enter a series of commands where you will see your robot moving around the board'
-  end
-
-  def show_command_instructions
-    puts <<~COMMAND_INSTRUCTIONS
-      Commands can be only either:
-      R, which means ask the robot to turn right from where it is currently facing
-      L, which means ask the robot to turn left from where it is currently facing
-      A, which means ask the robot to advance one cell from where it is facing
-      \n
-      Example command: RLAR
-      It means it command the robot to turn right, turn left, advance a step, and finish by turning right
-    COMMAND_INSTRUCTIONS
-  end
-
   def exit_or_proceed(input)
     input == 'exit' ? exit(true) : input
   end
 
-  def enter_another_command
-    puts 'Feel free to enter another set of commands'
+  def draw(screen)
+    puts '---------------------------------'
+    puts screen
+    puts '---------------------------------'
   end
 
-  def print_error(error)
-    puts error
+  def print(message)
+    puts message
   end
 
-  def print_board(board)
-    puts board
-    puts "\n\n"
+  def get_input_to_int
+    input = exit_or_proceed(gets.chomp)
+    input.scan(/\D/).any? ? input : input.to_i
   end
 
-  def print_outcome(robot, board, instruction)
-    puts "Current command: #{instruction}"
-    puts "\n"
-    puts board.board_with_axis
-    puts "\n"
-    puts "Robot coordinates: #{robot.coordinates}"
-    puts "Robot bearing: #{robot.bearing}"
+  def get_input_to_sym
+    exit_or_proceed(gets.chomp).to_sym
+  end
+
+  def get_input_as_str
+    exit_or_proceed(gets.chomp)
   end
 end
